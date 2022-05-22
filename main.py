dcf20289b3fb8add3ad83bfd6a1985afd6a628c2
ivan-ngchakming <ivan.ng.chak.ming@gmail.com>
2022-05-22 23:36:55 +0800

    fix bugs

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from bs4 import BeautifulSoup
 from tqdm import tqdm
 
 
-API_URL = "http://localhost:5000"
+API_URL = "http://localhost:8000"
 
 BASE_URL = "https://www.imdb.com"
 
-MAX_RESULT_PER_PAGE = 50
+MAX_RESULT_PER_PAGE = 250
 URL = f"{BASE_URL}/search/name/?adult=include&count=250"
 
 TOTAL = 11_922_407
@@ -34,7 +34,7 @@ def get_or_create_profile(data):
     )
     res = json.loads(r.text)
     if int(res["meta"]["total"]) >= 1:
-        return res["data"][0]
+        return res["data"][0], False
 
     profile_data = {
         "name": data["name"],
@@ -55,38 +55,36 @@ def get_or_create_profile(data):
         API_URL + "/profiles",
         json=profile_data,
     )
-    return json.loads(r.text)
+    return json.loads(r.text), True
 
 
 def main(start_page=0):
     with tqdm(total=TOTAL) as pbar:
-        with open('downloaded.txt', 'w+') as f:
-            downloaded = [line.split('\t')[1] for line in f.readlines()]
-            
-            for page in range(start_page, PAGES):
+        for page in range(start_page, PAGES):
 
-                r = requests.get(URL + f"start={page*MAX_RESULT_PER_PAGE+1}")
-                soup = BeautifulSoup(r.content, "html.parser")
+            r = requests.get(URL + f"&start={page*MAX_RESULT_PER_PAGE+1}")
+            soup = BeautifulSoup(r.content, "html.parser")
 
-                items = soup.find_all(attrs={"class": "lister-item mode-detail"})
+            items = soup.find_all(attrs={"class": "lister-item mode-detail"})
 
-                for item_idx, item in enumerate(items):
-                    pbar.set_description(f"{page}:{item_idx}")
-                    
+            for item_idx, item in enumerate(items):
+                pbar.set_description(f"{page}:{item_idx}")
+                
+                try:
                     image_url = get_full_img_url(item.img.get("src"))
                     actor_page_url = BASE_URL + item.a.get("href")
-
-                    if actor_page_url in downloaded:
-                        continue
                     
                     actor_page_r = requests.get(actor_page_url)
                     actor_page_soup = BeautifulSoup(actor_page_r.content, "html.parser")
 
                     # Name
                     name = actor_page_soup.title.text.replace(" - IMDb", "")
-                    firstname, *middlename, lastname = name.split(" ")
-                    middlename = " ".join(middlename)
-
+                    names = name.split(" ")
+                    if len(names) > 1:
+                        firstname, *middlename, lastname = names
+                        middlename = " ".join(middlename)
+                    else:
+                        firstname, middlename, lastname = name, None, None
                     pbar.set_description(f"{page}:{item_idx} - {name}")
                     
                     data = {
@@ -97,7 +95,7 @@ def main(start_page=0):
                         "source": actor_page_url,
                         "image": image_url,
                     }
-                        
+
                     # Birth date and place
                     born_info = actor_page_soup.find(id="name-born-info")
                     try:
@@ -110,9 +108,7 @@ def main(start_page=0):
                     except AttributeError:
                         pass
                     
-                    profile = get_or_create_profile(data)
-                    if profile["updated_date"] != profile["created_date"]:
-                        continue
+                    profile, created = get_or_create_profile(data)
 
                     r = requests.post(API_URL + "/photos", json={"url": data["image"]})
                     photo = json.loads(r.text)
@@ -122,9 +118,10 @@ def main(start_page=0):
                                 API_URL + f'/faces/{face["id"]}',
                                 json={"profile_id": profile["id"]},
                             )
-
-                    f.write(data["name"] + '\t' + actor_page_url + '\n')
-                    pbar.update(1)
+                except Exception:
+                    print(f"Error on {page}:{item_idx}")
+                
+                pbar.update(1)
 
 
 if __name__ == "__main__":
